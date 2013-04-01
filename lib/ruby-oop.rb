9616cc44fcd166e0ca5d729ac553da8b46ba6590
USAMI Kenta <tadsan@zonu.me>
2013-04-01 19:23:16 +0900

    Qiita Version

--- lib/ruby-oop.rb
+++ lib/ruby-oop.rb
@@ -1,7 +1,28 @@
-require "ruby-oop/version"
+#require "ruby-oop/version"
 
-module Ruby
-  module OOP
-    # Your code goes here...
-  end
-end
+Klass = ->(klass){
+  lex = {
+    methods: {},
+    class_methods: {
+      new: ->{}
+    },
+  }.merge(klass)
+  kvars = {}
+
+  return ->(source){
+    object = {source: source}
+    vars = {}
+    return ->(name, *as){lex[:methods][name][*[object]+as]}
+  }
+}
+
+StringWrapper = Klass[
+  name: 'StringWrapper',
+  methods: {
+    to_s: ->(slf){ slf[:source] }
+  },
+]
+
+str = StringWrapper["String"]
+
+p str[:to_s]
